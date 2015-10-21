a5f0b439906eed0c54e53a52e145519ffff286b4
Luis Felipe Perez <slashcat@gmail.com>
2015-10-20 23:08:55 -0500

    initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          // target.css file: source.less file
+          "assets/css/site.css": "assets/less/site.less"
+        }
+      }
+    },
+    uglify: {
+      site_js: {
+        options: {
+          mangle: true,
+          sourceMap: false,
+          compress: {
+            drop_console: true,
+            dead_code: true
+          }
+        },
+        files: {
+          'assets/js/app.min.js': ['assets/js/script.js']
+        }
+      }
+    },
+    watch: {
+      layout: {
+        files: ['*.html', 'assets/js/*.js'],
+        options: {
+          livereload: true
+        }
+      },
+      styles: {
+        // Which files to watch (all .less files recursively in the less directory)
+        files: ['assets/less/*.less', 'assets/less/**/*.less',],
+        tasks: ['less'],
+        options: {
+          nospawn: true,
+          livereload: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('assets', ['uglify', 'less']);
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+# Grunt + LESS Boilerplate Setup
+
+just clone this repository, change name, version, author and description to fit your project. Then just type
+
+		$ npm install
+		$ grunt watch
